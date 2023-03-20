e7e800bcdd16ade040625a219fefbca24507118f
Cat Buxton <128300268+Cat-Buxton@users.noreply.github.com>
2023-03-20 09:03:35 -0400

    Update EncoderAndDecoder-WithLabels.py

--- EncoderAndDecoder-WithLabels.py
+++ EncoderAndDecoder-WithLabels.py
@@ -48,7 +48,7 @@ while True:
                     # To convert the list to a string by joining the list together.
                     result = ''.join(message_e)
                     # Print the final result.
-                    print("Encoded message: " + result)
+                    print("ENCODED MESSAGE: " + result)
                 # If the inputted offset is not a digit, restart.
                 else:
                     print("Please input an integer, thank you.")
@@ -96,7 +96,7 @@ while True:
                     # To convert the list to a string by joining the list together.
                     result = ''.join(message_e)
                     # Print the final result.
-                    print("Decoded message: " + result)
+                    print("DECODED MESSAGE: " + result)
                 # If the inputted offset is not a digit, restart.
                 else:
                     print("Please input an integer, thank you.")
